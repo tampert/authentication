d727dcd82b107d84bd48fb7262832950a917455e
Dirk Soentjens <Dirk.Soentjens@gmail.com>
2023-03-02 10:34:47 +0100

    add validation

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ Playing around with authentication
 npm install
 npm run start
 ```
+
+
+```
+$ node
+> require("crypto").randomBytes(64).toString("hex")
+//This will generate a RANDOM string, you can cut paste this string in the .env file
+// Needed for ACCESS_TOKEN_SECRET and REFRESH_TOKEN_SECRET
+
+```
--- dbServer.js
+++ dbServer.js
@@ -3,7 +3,6 @@ const app = express()
 const mysql = require("mysql")
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
-app.use(express.json())
 
 require("dotenv").config()
 const DB_HOST = process.env.DB_HOST
@@ -13,19 +12,12 @@ const DB_DATABASE = process.env.DB_DATABASE
 const DB_PORT = process.env.DB_PORT
 const port = process.env.PORT
 
-// accessTokens
-function generateAccessToken(user) {
-    return
-    jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m" })
-}
 // refreshTokens
 let refreshTokens = []
-function generateRefreshToken(user) {
-    const refreshToken =
-        jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "20m" })
-    refreshTokens.push(refreshToken)
-    return refreshToken
-}
+
+app.use(express.json())
+app.listen(port,
+    () => console.log(`Server Started on port ${port}...`))
 
 const db = mysql.createPool({
     connectionLimit: 100,
@@ -39,8 +31,22 @@ db.getConnection((err, connection) => {
     if (err) throw (err)
     console.log("DB connected successful: " + connection.threadId)
 })
-app.listen(port,
-    () => console.log(`Server Started on port ${port}...`))
+
+
+// accessTokens
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m" })
+}
+
+function generateRefreshToken(user) {
+    const refreshToken =
+        jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "20m" })
+    refreshTokens.push(refreshToken)
+    return refreshToken
+}
+
+
+
 
 app.get("/", async (req, res) => {
     db.getConnection(async (err, connection) => {
@@ -55,16 +61,46 @@ app.get("/", async (req, res) => {
     })
 })
 
+app.get("/posts", validateToken, (req, res) => {
+    console.log("Token is valid")
+    console.log(req.user.user)
+    res.send(`${req.user.user} successfully accessed post`)
+})
+
+function validateToken(req, res, next) {
+    //get token from request header
+    const authHeader = req.headers["authorization"]
+    if (!authHeader) {
+        res.status(403).send("No authHeader")
+        return
+    }
+    const token = authHeader.split(" ")[1]
+    //the request header contains the token "Bearer <token>", split the string and use the second value in the split array.
+    if (token == null) res.sendStatus(400).send("Token not present")
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) {
+            res.status(403).send("Token invalid")
+        }
+        else {
+            req.user = user
+            next() //proceed to the next action in the calling function
+        }
+    }) //end of jwt.verify()
+} //end of function
+
 //CREATE USER
 app.post("/createUser", async (req, res) => {
+    if (req.body.name.length < 1) res.send("No name")
     const name = req.body.name;
+    if (req.body.password.length < 1) res.send("No password")
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const email = req.body.email || 'not set';
     db.getConnection(async (err, connection) => {
         if (err) throw (err)
         const sqlSearch = "SELECT * FROM users WHERE name = ?"
         const search_query = mysql.format(sqlSearch, [name])
-        const sqlInsert = "INSERT INTO users VALUES (0,?,?)"
-        const insert_query = mysql.format(sqlInsert, [name, hashedPassword])
+        const sqlInsert = "INSERT INTO users VALUES (0,?,?, ?)"
+        const insert_query = mysql.format(sqlInsert, [name, hashedPassword, email])
         // ? will be replaced by values
         // ?? will be replaced by string
         await connection.query(search_query, async (err, result) => {
@@ -109,8 +145,11 @@ app.post("/login", (req, res) => {
                 const hashedPassword = result[0].password
                 //get the hashedPassword from result
                 if (await bcrypt.compare(password, hashedPassword)) {
-                    console.log("---------> Login Successful")
-                    res.send(`${name} is logged in!`)
+                    console.log(`---------> Login Successful ${name} is logged in!`)
+
+                    const accessToken = generateAccessToken({ user: req.body.name })
+                    const refreshToken = generateRefreshToken({ user: req.body.name })
+                    res.json({ accessToken: accessToken, refreshToken: refreshToken })
                 }
                 else {
                     console.log("---------> Password Incorrect")
@@ -121,3 +160,17 @@ app.post("/login", (req, res) => {
     }) //end of db.connection()
 }) //end of app.post()
 
+
+app.post("/refreshToken", (req, res) => {
+    if (!refreshTokens.includes(req.body.token)) res.status(400).send("Refresh Token Invalid")
+    refreshTokens = refreshTokens.filter((c) => c != req.body.token)
+    const accessToken = generateAccessToken({ user: req.body.name })
+    const refreshToken = generateRefreshToken({ user: req.body.name })
+    res.json({ accessToken: accessToken, refreshToken: refreshToken })
+})
+
+app.delete("/logout", (req, res) => {
+    refreshTokens = refreshTokens.filter((c) => c != req.body.token)
+    res.send("Logged out!")
+    res.status(204).send("Logged out!")
+})
